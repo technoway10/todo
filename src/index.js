6859670747e07ca224ff8bc4cfe0658b9007c5d6
technoway10 <dcac7100@gmail.com>
2020-05-25 02:13:27 +0300

    2nd day

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-  const items = ['Drink Coffee', 'Build Awesome App'];
-  return (
-      <ul>
-        <li>{ items[0] }</li>
-        <li>{ items[1] }</li>
-      </ul>
-  );  
-};
-
-const AppHeader = () => {
-  return <h1>My Todo list</h1>;
-};
-
-const SearchPanel = () => {
-
-  const searchText = 'Type here to search';
-  const searchStyle = {
-    fontSize: '20px'
-  };
-
-  return <input
-    style={searchStyle} 
-    placeholder={searchText} />;
-};
+import AppHeader from './components/app-header';
+import TodoList from './components/todo-list';
+import SearchPanel from './components/search-panel';
 
 const App = () => {
 
