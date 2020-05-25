f6712ebbbc5cdabbc3eeeb6cc75b569e8f2dc8a0
technoway10 <dcac7100@gmail.com>
2020-05-26 02:20:44 +0300

    3rd day all

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+
+      const { id, ...itemProps } = item;
+
+      return (
+        <li key={id} className="list-group-item">
+           <TodoListItem { ...itemProps } />
+        </li>
+      );
+    });  
+
     return (
-        <ul>
-          <li><TodoListItem label="Drink Coffee" /></li>
-          <li><TodoListItem
-             label="Build React App"
-             important /></li>
+        <ul className="list-group todo-list">
+          { elements }
         </ul>
     );  
   };
