dbd1c7a0ecb00abc42dda1e5350b62751c47746e
technoway10 <dcac7100@gmail.com>
2020-05-26 02:18:20 +0300

    3rd day

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header';
 import TodoList from './components/todo-list';
 import SearchPanel from './components/search-panel';
+import ItemStatusFilter from './components/item-status-filter';
 
 const App = () => {
+
+    const todoData = [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id:3 }
+    ];
 
     return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />  
+      <TodoList todos={todoData} />
+      <ItemStatusFilter />  
     </div>
   );
 };
